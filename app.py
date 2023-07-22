b3b70a517698ca50fe4bf0f21b405433ee88e3f2
Sonny Agorvor-Otchie <Sonny4Sonnix@users.noreply.huggingface.co>
2023-07-22 17:54:58 +0000

    Create app.py

---
+++ app.py
@@ -0,0 +1,29 @@
+import pandas as pd 
+import gradio as gr
+import numpy as np
+import nltk
+import joblib
+from transformers import RobertaForSequenceClassification
+from transformers import AutoTokenizer
+from transformers import pipeline 
+#from transformers import utils
+import torch 
+
+
+#Load the model
+model = joblib.load("models/sentiment_analysis_model.joblib")
+
+#Function to predict sentiments from the input text using the model
+def predict_sentiment(text):
+    prediction = model.predict([text])[0]
+    if prediction==0:
+        return "Negative"
+    elif prediction == 1:
+        return "Neutral"
+    else:
+        return "Positive"
+
+
+# Create the Gradio app interface
+demo = gr.Interface(fn=predict_sentiment, input=gr.inputs.Textbox(),outputs="text")
+demo.launch()
