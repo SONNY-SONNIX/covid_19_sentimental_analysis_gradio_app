8ce59df3dd5c995dbfeeb46315a0b50e62100b83
Sonny Agorvor-Otchie <Sonny4Sonnix@users.noreply.huggingface.co>
2023-07-22 22:31:57 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,35 @@
+import gradio as gr 
+import transformers as pipeline 
+from transformers import AutoTokenizer,AutoModelForSequenceClassification
+from transformers import AutoModelForSequenceClassification, AutoTokenizer, pipeline
 
-import pandas as pd 
-import gradio as gr
-import numpy as np
-import nltk
-import joblib
+model_name = "Sonny4Sonnix/twitter-roberta-base-sentimental-analysis-of-covid-tweets"  # Replace with the name of the pre-trained model you want to use
+model = AutoModelForSequenceClassification.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+sentiment = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
+
+def get_sentiment(input_text):
+    return sentiment(input_text)
+
+
+
+#Function to predict sentiments from the input text using the model
+    prediction = model.predict([text])[0]
+    if label==-1:
+       return "Negative"
+    elif label== 0:
+        return "Neutral"
+    else:
+        return "Positive"
+
+iface = gr.Interface(fn=get_sentiment,title="Sentimental Analysis", inputs="text",outputs="text")
+iface.launch(inline=True)
+# import pandas as pd 
+# import gradio as gr
+# import numpy as np
+# import nltk
+# import joblib
 # import transformers
 # from transformers import RobertaForSequenceClassification
 # from transformers import AutoTokenizer
